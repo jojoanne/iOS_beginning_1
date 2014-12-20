1c5a49c43b7d8af1d12d11bddb6d461cb439d4c5
joanne <koongjoanne@gmail.com>
2014-12-19 20:57:13 -0800

    Initial Commit

---
+++ First/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  First
+//
+//  Created by Joanne Koong on 12/19/14.
+//  Copyright (c) 2014 jojoanne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ First/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  First
+//
+//  Created by Joanne Koong on 12/19/14.
+//  Copyright (c) 2014 jojoanne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
