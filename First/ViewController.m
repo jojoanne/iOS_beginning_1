aed66899f45291b36a50c706d4cc95138a0ff698
joanne <koongjoanne@gmail.com>
2014-12-20 08:59:01 -0800

    add comments

--- First/ViewController.m
+++ First/ViewController.m
@@ -24,6 +24,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+// use rgb randomization to generate color
 - (IBAction)changeColor:(id)sender {
     int r = arc4random() % 255;
     int g = arc4random() % 255;
@@ -31,7 +32,8 @@
     
     UIColor *color = [UIColor colorWithRed:(r/255.0) green:(g/255.0)
                                       blue:(b/255.0) alpha:1.0];
-    
+
+    // substantiate background color change
     [self.view setBackgroundColor:color];
 }
 
